0fb95139ad3bab88f30ad2b4b8dcf7cf5fdec478
Umarbek Rakhmatov <umarbek1306@gmail.com>
2023-02-24 17:33:37 -0500

    testy test

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title):
+        self.title = title
+        self._page_count = None
+    
+    def get_page_count(self):
+        return self._page_count
+
+    def set_page_count(self, new_page_count):
+        if type(new_page_count) != int:
+            print('page_count must be an integer')
+        else :
+            self._page_count = new_page_count
+    
+    page_count = property(get_page_count, set_page_count)
+
+    def turn_page(self):
+        print('Flipping the page...wow, you read fast!')
+
+
+if __name__ == '__main__':
+    book = Book('The Road')
+    book.set_page_count(272)
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand):
+        self.brand = brand
+        self.color = None
+        self._size = None
+        self.condition = None
+
+    def set_size(self, new_size):
+        if type(new_size) != int:
+            print("size must be an integer")
+        else:
+            self._size = new_size
+    def get_size(self):
+        return self._size
+    
+    size = property(get_size, set_size)
+
+    def cobble(self):
+        self.condition = "New"
+        print('Your shoe is as good as new!')
